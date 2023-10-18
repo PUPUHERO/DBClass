52be7e21f99235aa61dbc104590c016b03ca8067
PUPUHERO <victorwen17@gmail.com>
2023-10-19 01:21:15 +0800

    autorun now check the permission

--- scripts/autorun.ps1
+++ scripts/autorun.ps1
@@ -1,7 +1,21 @@
+
+# 檢查是否有管理員權限
+$isAdmin = [bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544")
+
+if (!$isAdmin) {
+    Write-Host "You need to run this script as an administrator."
+    pause
+    exit
+}
+
 # install choco
 .\choco\install_choco.ps1
+
+# install powershell and set powershell7 in context menu
 .\pwsh\install_pwsh_choco.ps1
 Start-Process regedit.exe -ArgumentList "/s pwsh\pwsh_open_here.reg" -Wait
+
+# install mysql
 .\mysql\install_mysql_choco.ps1
 .\mysql\set_mysql.ps1
 
