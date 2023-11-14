a946c8094a2fd9e4e9a89df6afa41d5cd51fec3d
PUPUHERO <victorwen17@gmail.com>
2023-11-14 13:44:28 +0800

    init hw04

---
+++ HW/hw04/1-a-insert.sql
@@ -0,0 +1,21 @@
+DELIMITER $$
+CREATE TRIGGER at_least_1500
+AFTER INSERT ON laptop
+FOR EACH ROW
+BEGIN
+	DECLARE avg_price DECIMAL(10, 2);
+
+	-- 計算新的平均值
+	SELECT AVG(price) INTO avg_price
+	FROM laptop;
+	
+	-- 檢查是否平均值低於 1500
+	IF avg_price < 1500 THEN
+	  -- 如果是，則刪除新增的數據
+		SIGNAL SQLSTATE '45000'
+		SET MESSAGE_TEXT = 'Error: average issue',
+		MYSQL_ERRNO = 1001;
+	END IF;
+END;
+$$
+DELIMITER ;
---
+++ HW/hw04/1-b-pc.sql
@@ -0,0 +1,24 @@
+DELIMITER $$
+CREATE TRIGGER no_exist_pc
+BEFORE INSERT ON pc
+FOR EACH ROW
+BEGIN
+	IF EXISTS (SELECT 1 FROM laptop where model = NEW.model) THEN
+     -- 如果存在，可以執行相應的操作，例如拋出錯誤或選擇其他操作
+     SIGNAL SQLSTATE '45000'
+     SET MESSAGE_TEXT = 'Data already exists in the laptop table',
+     MYSQL_ERRNO = 1001;
+   ELSEIF EXISTS (SELECT 1 FROM pc where model = NEW.model) THEN
+     -- 如果存在，可以執行相應的操作，例如拋出錯誤或選擇其他操作
+     SIGNAL SQLSTATE '45000'
+     SET MESSAGE_TEXT = 'Data already exists in the pc table',
+     MYSQL_ERRNO = 1001;
+   ELSEIF EXISTS (SELECT 1 FROM printer where model = NEW.model) THEN
+     -- 如果存在，可以執行相應的操作，例如拋出錯誤或選擇其他操作
+     SIGNAL SQLSTATE '45000'
+     SET MESSAGE_TEXT = 'Data already exists in the printer table',
+     MYSQL_ERRNO = 1001;
+   END IF;
+END;
+$$
+DELIMITER ;
